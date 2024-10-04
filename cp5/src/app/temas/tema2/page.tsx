b34bad93f03198ba74504dcad2ef13434ca273da
LeonardoIafrate <leonardoiafrate01@gmail.com>
2024-10-04 16:45:05 -0300

    Page do layout Tema2 textos feitos

---
+++ cp5/src/app/temas/tema2/page.tsx
@@ -0,0 +1,10 @@
+export default function Tema2(){
+    return (
+        <div>
+            <h1>Vênus, O Planeta que Nasceu do Caos</h1>
+            <p>Segundo Velikovsky, Vênus não era o planeta calmo e brilhante que vemos hoje à noite. Em vez disso, era um cometa recém-nascido, ejetado das profundezas de Júpiter, vagando pelo Sistema Solar em uma órbita caótica. O caos começou quando esse cometa passou perigosamente perto da Terra, desencadeando desastres naturais em todo o planeta. Velikovsky ligou o surgimento de Vênus a eventos históricos, como as Pragas do Egito e a separação do Mar Vermelho, ambos retratados no Êxodo. Ele sugeriu que o que muitas vezes é descrito como milagres religiosos eram, na verdade, consequências de interações celestiais.
+            Segundo ele, Vênus causou terremotos, erupções vulcânicas e tempestades de fogo, deixando as civilizações antigas em choque e terror, documentando essas catástrofes em textos sagrados como a Bíblia.
+            </p>
+        </div>
+    )
+}
