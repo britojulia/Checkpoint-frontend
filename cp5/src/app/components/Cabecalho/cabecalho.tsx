048da17d96069e7a3e4da44879a441ec84e0b086
britojulia <juliabri1234@gmail.com>
2024-10-04 15:14:37 -0300

    Adicionando texto

--- cp5/src/app/components/Cabecalho/cabecalho.tsx
+++ cp5/src/app/components/Cabecalho/cabecalho.tsx
@@ -2,9 +2,10 @@ import Menu from "../Menu/menu";
 
 export default function Cabecalho(){
     return(
-       <header className="cabecalho">
-           <h1>Words in Collision - Immanuel Velikovsky</h1>
-           <Menu/>
-       </header> 
+        <header className="cabecalho">
+            <h1>Words in Collision - Immanuel Velikovsky</h1>
+            <Menu/>
+            <h3>Immanuel Velikovsky 1895-1979 foi um escritor e psicanalista conhecido por teorias controversas em história e cosmologia. Em seu livro Worlds in Collision (1950), ele sugeriu que eventos catastróficos, como descritos em mitos e textos religiosos, foram causados por interações de planetas como Vênus e Marte com a Terra. Essas ideias foram rejeitadas pela ciência por falta de evidências, mas tiveram impacto cultural e geraram debates.</h3>
+        </header> 
     )
 };
