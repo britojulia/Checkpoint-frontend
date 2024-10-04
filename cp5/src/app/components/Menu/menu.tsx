8c6e3dd9e1abddf457dd087dff7b328f87139051
gustavoreislima13 <reisanselmo7@gmail.com>
2024-10-04 17:12:38 -0300

    menu

--- cp5/src/app/components/Menu/menu.tsx
+++ cp5/src/app/components/Menu/menu.tsx
@@ -6,9 +6,9 @@ export default function Menu() {
       <ul className="flex space-x-8">
         <li><Link href="/" className="text-white hover:text-gray-400">Home</Link></li>
         <li><Link href="/temas/tema1" className="text-white hover:text-gray-400">Teoria de Catástrofes Cósmicas</Link></li>
-        <li><Link href="/temas/tema2" className="text-white hover:text-gray-400">Tema1</Link></li>
+        <li><Link href="/temas/tema2" className="text-white hover:text-gray-400">Vênus, O Planeta que Nasceu do Caos</Link></li>
         <li><Link href="/temas/tema3" className="text-white hover:text-gray-400">As Guerras dos Deuses e Marte</Link></li>
-        <li><Link href="/temas/tema4" className="text-white hover:text-gray-400">Tema2s</Link></li>
+        <li><Link href="/temas/tema4" className="text-white hover:text-gray-400">Marte, o Deus da Guerra Celestial </Link></li>
       </ul>
     </nav>
   );
