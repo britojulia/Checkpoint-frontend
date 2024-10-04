c196f404a7f482984b6ec6deedbdc48d41ebb6ed
gustavoreislima13 <reisanselmo7@gmail.com>
2024-10-04 17:04:01 -0300

    Teoria de Catástrofes Cósmicas arrumada

--- cp5/src/app/components/Cabecalho/cabecalho.tsx
+++ cp5/src/app/components/Cabecalho/cabecalho.tsx
@@ -5,7 +5,7 @@ export default function Cabecalho(){
         <header className="cabecalho">
             <h1>Words in Collision - Immanuel Velikovsky</h1>
             <Menu/>
-            <h3>Immanuel Velikovsky 1895-1979 foi um escritor e psicanalista conhecido por teorias controversas em história e cosmologia. Em seu livro Worlds in Collision (1950), ele sugeriu que eventos catastróficos, como descritos em mitos e textos religiosos, foram causados por interações de planetas como Vênus e Marte com a Terra. Essas ideias foram rejeitadas pela ciência por falta de evidências, mas tiveram impacto cultural e geraram debates.</h3>
+            <h3></h3>
         </header> 
     )
 };
--- cp5/src/app/components/Menu/menu.tsx
+++ cp5/src/app/components/Menu/menu.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
 export default function Menu() {
-    return (
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/temas/tema1">Teoria de Catástrofes Cósmicas</Link></li>
-                <li><Link href="/temas/tema2">Tema1</Link></li>
-                <li><Link href="/temas/tema3">As Guerras dos Deuses e Marte</Link></li>
-                <li><Link href="/temas/demostracao">D</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="flex justify-center">
+      <ul className="flex space-x-8">
+        <li><Link href="/" className="text-white hover:text-gray-400">Home</Link></li>
+        <li><Link href="/temas/tema1" className="text-white hover:text-gray-400">D</Link></li>
+        <li><Link href="/temas/tema2" className="text-white hover:text-gray-400">Tema1</Link></li>
+        <li><Link href="/temas/tema3" className="text-white hover:text-gray-400">As Guerras dos Deuses e Marte</Link></li>
+        <li><Link href="/temas/tema4" className="text-white hover:text-gray-400">Teoria de Catástrofes Cósmicas</Link></li>
+      </ul>
+    </nav>
+  );
 }
